9447609bd25777223d2a3b08d9aaefce9fcdb319
Ali Zarghami <alizarghami@gmail.com>
2024-09-15 12:46:21 -0700

    Add Dataset transform script

---
+++ transform_dataset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+original_path = "../crowdsourced-datasets/my-dataset/binary_labeled_data.csv"
+transformed_path = "../crowdsourced-datasets/my-dataset/transformed_dataset.csv"
+
+
+df = pd.read_csv(original_path)
+
+df_melted = pd.melt(df, id_vars=['participant', 'timestamp'], value_vars=['verbal_binary_label', 'activpal_binary_label', 'google_binary_label'],
+                    var_name='workerID', value_name='response')
+
+df_melted['goldLabel'] = None
+df_melted.rename(columns={'participant': 'taskID'}, inplace=True)
+
+new_df = df_melted[['workerID', 'taskID', 'response', 'goldLabel', 'timestamp']]
+
+new_df.to_csv(transformed_path, index=False)
