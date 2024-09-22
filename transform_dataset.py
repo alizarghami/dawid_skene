3d3b1c6eeb8525dbab382b18c23173b1f67fa903
Hossein Khayami <h.khayami@gmail.com>
2024-09-21 22:37:42 -0400

    add consensus as gold labels

--- transform_dataset.py
+++ transform_dataset.py
@@ -6,10 +6,11 @@ transformed_path = "../crowdsourced-datasets/my-dataset/transformed_dataset.csv"
 
 df = pd.read_csv(original_path)
 
-df_melted = pd.melt(df, id_vars=['participant', 'timestamp'], value_vars=['verbal_binary_label', 'activpal_binary_label', 'google_binary_label'],
+df_melted = pd.melt(df, id_vars=['participant', 'timestamp', 'consensus_binary_label'], value_vars=['verbal_binary_label', 'activpal_binary_label', 'google_binary_label'],
                     var_name='workerID', value_name='response')
 
-df_melted['goldLabel'] = None
+# df_melted['goldLabel'] = None
+df_melted.rename(columns={'consensus_binary_label': 'goldLabel'}, inplace=True)
 df_melted.rename(columns={'participant': 'taskID'}, inplace=True)
 
 new_df = df_melted[['workerID', 'taskID', 'response', 'goldLabel', 'timestamp']]
